657e4bd1bf246aee922d1ccca9c472116d8ca39b
Diego <dturiso@yahoo.com>
2017-12-02 22:15:10 +0100

    Profile - update

--- UserFront/src/main/java/com/userfront/controller/UserController.java
+++ UserFront/src/main/java/com/userfront/controller/UserController.java
@@ -5,6 +5,7 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,11 +21,25 @@ public class UserController {
 	
 	@RequestMapping(value = "/profile", method = RequestMethod.GET)
 	public String profile(Model model, Principal principal) {
-		
 		User user = userService.findByUsername(principal.getName());
 		model.addAttribute(user);
 		
 		return "profile";
 	}
 	
+	@RequestMapping(value = "/profile", method = RequestMethod.POST)
+	public String profilePOST(@ModelAttribute("user") User newUser, Model model) {
+		User user = userService.findByUsername(newUser.getUsername());
+		user.setUsername(newUser.getUsername());
+		user.setFirstName(newUser.getFirstName());
+		user.setLastName(newUser.getLastName());
+		user.setEmail(newUser.getEmail());
+		user.setPhone(newUser.getPhone());
+		
+		model.addAttribute("user", user);
+		
+		userService.saveUser(user);
+		
+		return "profile";
+	}
 }
--- UserFront/src/main/java/com/userfront/service/impl/UserServiceImpl.java
+++ UserFront/src/main/java/com/userfront/service/impl/UserServiceImpl.java
@@ -92,4 +92,7 @@ public class UserServiceImpl implements UserService {
         return false;
     }
 
+    public User saveUser(User user) {
+    	return userDao.save(user);
+    }
 }
